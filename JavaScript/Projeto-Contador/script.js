19a6ccfd502cb32085cd66a8b310088db6c81189
Naã <contatonaacomtio@gmail.com>
2023-04-23 20:35:55 -0300

    Exercises

---
+++ JavaScript/Projeto-Contador/script.js
@@ -0,0 +1,25 @@
+function contar() {
+    var iini = Number(document.querySelector('input#iini').value)
+    var ifim = Number(document.querySelector('input#ifim').value)
+    var ipas = Number(document.querySelector('input#ipas').value)
+    var res = document.querySelector('div#res')
+    window.alert(`iini é ${iini}, ifim é ${ifim}, ipas é ${ipas}`)
+    res.style.textAlign = 'center'
+    if (ipas == 0) {
+        window.alert('[ERRO] Passo inválido, considerando passo "1"!')
+        var ipas = 1
+    }
+    if (ifim > iini) {
+        for (iini; iini <= ifim; iini += ipas) {
+            res.innerHTML += `${iini} -> `
+        }
+        res.innerHTML += `Acabou! `
+    } else if (ifim < iini) {
+        for (iini; iini >= ifim; iini -= ipas) {
+            res.innerHTML += `${iini} -> `
+        } 
+        res.innerHTML += `Acabou! `
+    } else {
+        res.innerHTML += `Impossível contar! `
+    }
+}
