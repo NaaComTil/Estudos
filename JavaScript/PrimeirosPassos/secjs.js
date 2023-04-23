56991a8a47a74600f104973d0d23055f347a0e9a
Naã <contatonaacomtio@gmail.com>
2023-04-23 16:39:51 -0300

    aa

---
+++ JavaScript/PrimeirosPassos/secjs.js
@@ -0,0 +1,18 @@
+// Estruturas de repetição:
+
+// while (condição) {
+//  
+//}
+
+// var c = 1
+// do {
+//  
+// } while (condição)
+
+// for (c=2; c=10; c += 2) {
+//
+// }
+
+//
+//
+//
--- Python/Mundos/Mundo 3/Exer.py
+++ Python/Mundos/Mundo 3/Exer.py
@@ -1,8 +1,3 @@
-print('Pegue o ônibus!')
-km = int(input('Qual distancia em Km? '))
-if km < 200:
-    print(f"""Você irá andar {km}kms, e o preço por cada Km abaixo de 200kms é R$0.50,
-    logo {km}*0.50 dá R${0.50*km}!""")
-else:
-    print(f"""Você irá andar {km}kms, e o preço por cada Km acima de 200kms é R$0.45,
-    logo {km}*0.45 dá R${0.45*km}!""".format(km,km,0.45*km))
+for c in range(1, 10, 1):
+    print(c)
+    c += 1
