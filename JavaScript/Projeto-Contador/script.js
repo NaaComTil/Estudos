e29971e97de73b3ea8b400e8136f4cef12c6acfe
Naã <contatonaacomtio@gmail.com>
2023-04-24 08:51:36 -0300

    Fixed

--- JavaScript/Projeto-Contador/script.js
+++ JavaScript/Projeto-Contador/script.js
@@ -2,24 +2,25 @@ function contar() {
     var iini = Number(document.querySelector('input#iini').value)
     var ifim = Number(document.querySelector('input#ifim').value)
     var ipas = Number(document.querySelector('input#ipas').value)
-    var res = document.querySelector('div#res')
+    var res = document.querySelector('p#resp')
     window.alert(`iini é ${iini}, ifim é ${ifim}, ipas é ${ipas}`)
     res.style.textAlign = 'center'
+    res.innerHTML = ``
     if (ipas == 0) {
         window.alert('[ERRO] Passo inválido, considerando passo "1"!')
         var ipas = 1
     }
     if (ifim > iini) {
         for (iini; iini <= ifim; iini += ipas) {
-            res.innerHTML += `${iini} -> `
+            res.innerHTML += `${iini} \u{1F449} `
         }
-        res.innerHTML += `Acabou! `
+        res.innerHTML += `Acabou! \u{1F3C1} `
     } else if (ifim < iini) {
         for (iini; iini >= ifim; iini -= ipas) {
             res.innerHTML += `${iini} \u{1F449} `
         } 
         res.innerHTML += `Acabou! \u{1F3C1} `
     } else {
-        res.innerHTML += `Impossível contar! `
+        res.innerHTML += `Impossível contar! Acabou! \u{1F3C1} `
     }	
 }
