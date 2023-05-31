4586a95e773c69fe5c53087153afc03eef50cd59
Naã <contatonaacomtio@gmail.com>
2023-05-31 17:31:59 -0300

    a

--- JavaScript/Projeto-Idade/script.js
+++ JavaScript/Projeto-Idade/script.js
@@ -7,7 +7,6 @@ function verificar() {
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        window.alert('Tudo ok!')
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
---
+++ Temp/scripts/scripts.js
@@ -0,0 +1,5 @@
+var pag = 0
+function prox() {
+    var pag =+ 1
+    window.location.href = 'pag'+pag+'.html'
+}
